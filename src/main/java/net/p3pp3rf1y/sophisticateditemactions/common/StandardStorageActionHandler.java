fa22870657cf7f8ffb86ae7f4431da6e55520bb4
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-01-22 21:10:14 +0100

    fix: 🐛 Fixed to prevent players from being considered target for any of item actions so highlighting players with item in their inventory, depositing to them or restocking from them isn't a thing after this change.

--- src/main/java/net/p3pp3rf1y/sophisticateditemactions/common/StandardStorageActionHandler.java
+++ src/main/java/net/p3pp3rf1y/sophisticateditemactions/common/StandardStorageActionHandler.java
@@ -4,6 +4,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
@@ -34,7 +35,7 @@ public class StandardStorageActionHandler implements IBlockItemActionHandler, IE
 
 	@Override
 	public boolean canActOn(Entity entity) {
-		return entity.getCapability(Capabilities.Item.ENTITY, null) != null;
+		return !(entity instanceof Player) && entity.getCapability(Capabilities.Item.ENTITY, null) != null;
 	}
 
 	@Override
