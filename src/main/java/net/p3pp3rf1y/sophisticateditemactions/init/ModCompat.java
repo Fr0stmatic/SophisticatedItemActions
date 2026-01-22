571e68a9c86c427081694ab5117ac827117b06d1
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-01-22 19:57:11 +0100

    feat: ✨ Added REI compatibility for highlight and restock based on its ingredient lists / recipe view

--- src/main/java/net/p3pp3rf1y/sophisticateditemactions/init/ModCompat.java
+++ src/main/java/net/p3pp3rf1y/sophisticateditemactions/init/ModCompat.java
@@ -5,6 +5,7 @@ import net.p3pp3rf1y.sophisticatedcore.compat.CompatModIds;
 import net.p3pp3rf1y.sophisticatedcore.compat.CompatRegistry;
 import net.p3pp3rf1y.sophisticateditemactions.compat.recipeviewers.emi.EmiCompat;
 import net.p3pp3rf1y.sophisticateditemactions.compat.recipeviewers.jei.JeiCompat;
+import net.p3pp3rf1y.sophisticateditemactions.compat.recipeviewers.rei.ReiCompat;
 import net.p3pp3rf1y.sophisticateditemactions.compat.sophisticatedstorageinmotion.StorageInMotionCompat;
 
 import static net.p3pp3rf1y.sophisticateditemactions.compat.CompatModIds.STORAGE_IN_MOTION;
@@ -15,6 +16,7 @@ public class ModCompat {
 	public static void register() {
 		CompatRegistry.registerCompat(new CompatInfo(CompatModIds.JEI), () -> modBus -> new JeiCompat());
 		CompatRegistry.registerCompat(new CompatInfo(CompatModIds.EMI), () -> modBus -> new EmiCompat());
+		CompatRegistry.registerCompat(new CompatInfo(CompatModIds.REI), () -> modBus -> new ReiCompat());
 		CompatRegistry.registerCompat(new CompatInfo(STORAGE_IN_MOTION), () -> modBus -> new StorageInMotionCompat());
 	}
 }
