8de56849ef84e4ced24ca99eff9107e1f5c5eaa6
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-01-23 11:42:41 +0100

    fix: 🐛 Fixed restocking from JEI/EMI/REI to fill existing stack of an item before filling an empty slot in player's inventory

--- src/main/java/net/p3pp3rf1y/sophisticateditemactions/compat/recipeviewers/jei/JeiClientCompat.java
+++ src/main/java/net/p3pp3rf1y/sophisticateditemactions/compat/recipeviewers/jei/JeiClientCompat.java
@@ -7,7 +7,6 @@ import mezz.jei.api.runtime.IJeiRuntime;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
@@ -40,11 +39,6 @@ public class JeiClientCompat {
 			public boolean restockSingle(Screen screen) {
 				//in case of crafting grid return single
 				return runtime != null && runtime.getRecipesGui().getIngredientUnderMouse(VanillaTypes.ITEM_STACK).isPresent();
-			}
-
-			@Override
-			public int getRestockSlot(Screen screen, Player player) {
-				return player.getInventory().getFreeSlot();
 			}
 
 			@Override
