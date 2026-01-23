ee9cd61de7466c1d3116153082b3e59ea5a8c828
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-01-23 14:52:44 +0100

    fix: 🐛 Fixed to not highlight controller when an item is found in one of the storages in its multiblock. Instead just the storage itself is highlighted.

--- src/main/java/net/p3pp3rf1y/sophisticateditemactions/common/ControllableStorageItemActionHandler.java
+++ src/main/java/net/p3pp3rf1y/sophisticateditemactions/common/ControllableStorageItemActionHandler.java
@@ -15,7 +15,7 @@ public class ControllableStorageItemActionHandler implements IBlockEntityItemAct
 	public static final ResourceLocation ID = SophisticatedCore.getRL("controllable_storage");
 
 	@Override
-	public ItemMatchResult getItemMatch(ItemStackKey stackKey, IControllableStorage storage) {
+	public ItemMatchResult getItemMatch(ItemStackKey stackKey, IControllableStorage storage, Action action) {
 		ISlotTracker slotTracker = storage.getStorageWrapper().getInventoryHandler().getSlotTracker();
 		return getItemMatchResult(stackKey, slotTracker, false);
 	}
