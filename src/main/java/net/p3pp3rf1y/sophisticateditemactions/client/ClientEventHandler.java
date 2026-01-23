2e2503bf36220ac100242bee974b451b9bd1a2c9
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-01-23 11:42:41 +0100

    fix: 🐛 Fixed restocking from JEI/EMI/REI to fill existing stack of an item before filling an empty slot in player's inventory

--- src/main/java/net/p3pp3rf1y/sophisticateditemactions/client/ClientEventHandler.java
+++ src/main/java/net/p3pp3rf1y/sophisticateditemactions/client/ClientEventHandler.java
@@ -6,6 +6,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.core.NonNullList;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.Slot;
@@ -55,7 +56,7 @@ public class ClientEventHandler {
 		}
 
 		@Override
-		public int getRestockSlot(Screen screen, Player player) {
+		public int getRestockSlot(Screen screen, Player player, ItemStack filter) {
 			if (!(screen instanceof AbstractContainerScreen<?> containerScreen) || containerScreen.getSlotUnderMouse() == null) {
 				return -1;
 			}
@@ -167,7 +168,7 @@ public class ClientEventHandler {
 			IHoveredStackProvider provider = getHoveredStackProvider(screen);
 			if (provider != null) {
 				filter = provider.getHoveredStack(screen);
-				slot = provider.getRestockSlot(screen, player);
+				slot = provider.getRestockSlot(screen, player, filter);
 				fillEmpty |= provider.restockEmptySlot();
 				refillSingle = provider.restockSingle(screen);
 			}
@@ -181,7 +182,7 @@ public class ClientEventHandler {
 		}
 
 		if (mainInventory || hotbar) {
-			ItemTransferHandler.restockMultipleItems(player, filter, mainInventory, hotbar, fillEmpty, false);
+			ItemTransferHandler.restockMultipleItems(player, filter, mainInventory, hotbar, fillEmpty, refillSingle);
 		} else {
 			ItemTransferHandler.restockItem(player, filter, slot, fillEmpty, refillSingle);
 		}
@@ -263,7 +264,17 @@ public class ClientEventHandler {
 
 		boolean restockSingle(Screen screen);
 
-		int getRestockSlot(Screen screen, Player player);
+		default int getRestockSlot(Screen screen, Player player, ItemStack filter) {
+			NonNullList<ItemStack> items = player.getInventory().getNonEquipmentItems();
+			for(int slot = 0; slot < items.size(); ++slot) {
+				ItemStack stack = items.get(slot);
+				if (!stack.isEmpty() && ItemStack.isSameItemSameComponents(filter, stack) && stack.getCount() < stack.getMaxStackSize()) {
+					return slot;
+				}
+			}
+
+			return player.getInventory().getFreeSlot();
+		}
 
 		default boolean restockEmptySlot() {
 			return false;
